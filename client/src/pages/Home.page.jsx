03276c74e5fad7a715481dbb99ce67cc4f33e135
Talya Gabso <talyagabso@gmail.com>
2021-05-20 16:04:25 +0300

    .

--- client/src/pages/Home.page.jsx
+++ client/src/pages/Home.page.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 // Components
 import TextureCard from '../components/TextureCard/TextureCard.component.jsx'
-
 // CSS
+import Typography from '@material-ui/core/Typography'
 
 
 const HomePage = () => {
@@ -13,7 +13,7 @@ const HomePage = () => {
    return (
       <div className='homepage'>
          <h1>The Curls Beauty Supply Store</h1>
-         <h4>a sentence about the curls beauty supply store mission</h4>
+         <Typography variant="h4">a sentence about the curls beauty supply store mission</Typography>
          <TextureCard />
       </div>
    );
